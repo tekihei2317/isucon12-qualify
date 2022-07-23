e68a71fae9c0c218c9d976423f4fd720710c832d
tekihei2317 <tekihei2317@gmail.com>
2022-07-23 07:13:47 +0000

    scoreとplayerをjoin 3101

--- node/src/use-case/get-player-ranking.ts
+++ node/src/use-case/get-player-ranking.ts
@@ -10,6 +10,13 @@ type CompetitionRankingResult = {
   ranks: CompetitionRank[]
 }
 
+interface JoinedPlayerScoreRow {
+  player_id: string
+  score: number
+  row_num: number
+  display_name: string
+}
+
 export async function getPlayerRanking (req: Request, res: Response)  {
   try {
     const viewer = await parseViewer(req)
@@ -60,13 +67,10 @@ export async function getPlayerRanking (req: Request, res: Response)  {
       // player_scoreを読んでいるときに更新が走ると不整合が起こるのでロックを取得する
       const unlock = await flockByTenantID(tenant.id)
       try {
-        // const pss = await tenantDB.all<PlayerScoreRow[]>(
-        //   'SELECT * FROM player_score WHERE tenant_id = ? AND competition_id = ? ORDER BY row_num DESC',
-        //   tenant.id,
-        //   competition.id
-        // )
-        const pss = await tenantDB.all<PlayerScoreRow[]>(
-          'SELECT player_id, MAX(row_num) as row_num, score FROM player_score WHERE tenant_id = ? AND competition_id = ? GROUP BY player_id',
+        const pss = await tenantDB.all<JoinedPlayerScoreRow[]>(
+          'SELECT ps.player_id as player_id, MAX(ps.row_num) as row_num, ps.score as score, player.display_name '+
+          'FROM player_score AS ps LEFT JOIN player ON player.id = ps.player_id '+
+          'WHERE ps.tenant_id = ? AND ps.competition_id = ? GROUP BY ps.player_id',
           tenant.id,
           competition.id
         )
@@ -74,22 +78,11 @@ export async function getPlayerRanking (req: Request, res: Response)  {
         // const scoredPlayerSet: { [player_id: string]: number } = {}
         const tmpRanks: (CompetitionRank & WithRowNum)[] = []
         for (const ps of pss) {
-          // player_scoreが同一player_id内ではrow_numの降順でソートされているので
-          // 現れたのが2回目以降のplayer_idはより大きいrow_numでスコアが出ているとみなせる
-          // if (scoredPlayerSet[ps.player_id]) {
-          //   continue
-          // }
-          // scoredPlayerSet[ps.player_id] = 1
-          const p = await retrievePlayer(tenantDB, ps.player_id)
-          if (!p) {
-            throw new Error('error retrievePlayer')
-          }
-
           tmpRanks.push({
             rank: 0,
             score: ps.score,
-            player_id: p.id,
-            player_display_name: p.display_name,
+            player_id: ps.player_id,
+            player_display_name: ps.display_name,
             row_num: ps.row_num,
           })
         }
